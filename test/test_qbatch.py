0c5c399fa96d7f334c2ae8b9c17d2cae15f60f5b
Jon Pipitone <jon@pipitone.ca>
2016-03-09 21:48:47 -0500

    Added PBS dry-run test

--- test/test_qbatch.py
+++ test/test_qbatch.py
@@ -57,6 +57,30 @@ def test_run_qbatch_sge_dryrun_array_piped_chunks():
       assert out == expected, \
         "Chunk {}: Expected {} but got {}".format(chunk, expected, out)
 
+def test_run_qbatch_pbs_dryrun_array_piped_chunks(): 
+    chunk_size = 10
+    chunks = 5
+    outputs = range(chunk_size*chunks)
+
+    cmds = "\n".join(map(lambda x: 'echo {}'.format(x), outputs))
+    p = command_pipe('qbatch -n -b pbs -c {}'.format(chunk_size))
+    out, err = p.communicate(cmds)
+
+    array_script = join(tempdir, 'STDIN.array')
+    assert p.returncode == 0
+    assert exists(array_script)
+
+    for chunk in range(1, chunks+1):
+      os.environ['PBS_ARRAYID'] = str(chunk)
+      expected = '\n'.join(map(str, outputs[(chunk-1)*chunk_size:chunk*chunk_size])) + '\n'
+      array_pipe = command_pipe(array_script)
+      out, _ = array_pipe.communicate()
+
+      assert array_pipe.returncode == 0, \
+        "Chunk {}: return code = {}".format(chunk, array_pipe.returncode)
+      assert out == expected, \
+        "Chunk {}: Expected {} but got {}".format(chunk, expected, out)
+
 def test_run_qbatch_local_piped_commands(): 
     cmds = "\n".join(["echo hello"]*24)
     p = command_pipe('qbatch -b local')
@@ -64,5 +88,5 @@ def test_run_qbatch_local_piped_commands():
 
     expected, _ = command_pipe('bash').communicate(cmds)
 
-    assert p.returncode == 0 
+    assert p.returncode == 0, err
     assert out == expected, out
