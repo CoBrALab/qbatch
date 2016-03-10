c30e82ab0ad104366bd231dca0f93906825de656
Jon Pipitone <jon@pipitone.ca>
2016-03-09 22:31:28 -0500

    pep8 compliance

--- test/test_qbatch.py
+++ test/test_qbatch.py
@@ -1,42 +1,45 @@
 from os.path import join, exists
-import glob 
-import nose
 import os
 import shutil
 import shlex
 from subprocess import Popen, PIPE
 import tempfile
 
-tempdir = None 
+tempdir = None
 
 # set this to folder that all nodes on the cluster have access to
-SHARED_FOLDER=os.getcwd() 
+SHARED_FOLDER = os.getcwd()
 
-def setup_module(): 
-    global tempdir 
+
+def setup_module():
+    global tempdir
     tempdir = tempfile.mkdtemp(dir=SHARED_FOLDER)
     os.environ["QBATCH_SCRIPT_FOLDER"] = tempdir
     os.environ["QBATCH_PPN"] = "1"
-    os.environ["PATH"] = "./scripts:" + os.environ["PATH"] # HACK
+    os.environ["PATH"] = "./scripts:" + os.environ["PATH"]  # HACK
 
-def teardown_module(): 
+
+def teardown_module():
     shutil.rmtree(tempdir)
+
 
 def command_pipe(command):
     return Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-def test_run_qbatch_dryrun_array_output_exists(): 
+
+def test_run_qbatch_dryrun_array_output_exists():
     cmds = "\n".join(["echo hello"])
     p = command_pipe('qbatch -n')
     out, err = p.communicate(cmds)
 
-    assert p.returncode == 0 
+    assert p.returncode == 0
     assert exists(join(tempdir, 'STDIN.array'))
 
-def test_run_qbatch_sge_dryrun_array_piped_chunks(): 
+
+def test_run_qbatch_sge_dryrun_array_piped_chunks():
     chunk_size = 10
     chunks = 5
-    outputs = range(chunk_size*chunks)
+    outputs = range(chunk_size * chunks)
 
     cmds = "\n".join(map(lambda x: 'echo {}'.format(x), outputs))
     p = command_pipe('qbatch -n -b sge -c {}'.format(chunk_size))
@@ -46,21 +49,24 @@ def test_run_qbatch_sge_dryrun_array_piped_chunks():
     assert p.returncode == 0
     assert exists(array_script)
 
-    for chunk in range(1, chunks+1):
-      os.environ['SGE_TASK_ID'] = str(chunk)
-      expected = '\n'.join(map(str, outputs[(chunk-1)*chunk_size:chunk*chunk_size])) + '\n'
-      array_pipe = command_pipe(array_script)
-      out, _ = array_pipe.communicate()
+    for chunk in range(1, chunks + 1):
+        os.environ['SGE_TASK_ID'] = str(chunk)
+        expected = '\n'.join(
+            map(str,
+                outputs[(chunk - 1) * chunk_size:chunk * chunk_size])) + '\n'
+        array_pipe = command_pipe(array_script)
+        out, _ = array_pipe.communicate()
 
-      assert array_pipe.returncode == 0, \
-        "Chunk {}: return code = {}".format(chunk, array_pipe.returncode)
-      assert out == expected, \
-        "Chunk {}: Expected {} but got {}".format(chunk, expected, out)
+        assert array_pipe.returncode == 0, \
+            "Chunk {}: return code = {}".format(chunk, array_pipe.returncode)
+        assert out == expected, \
+            "Chunk {}: Expected {} but got {}".format(chunk, expected, out)
 
-def test_run_qbatch_pbs_dryrun_array_piped_chunks(): 
+
+def test_run_qbatch_pbs_dryrun_array_piped_chunks():
     chunk_size = 10
     chunks = 5
-    outputs = range(chunk_size*chunks)
+    outputs = range(chunk_size * chunks)
 
     cmds = "\n".join(map(lambda x: 'echo {}'.format(x), outputs))
     p = command_pipe('qbatch -n -b pbs -c {}'.format(chunk_size))
@@ -70,19 +76,22 @@ def test_run_qbatch_pbs_dryrun_array_piped_chunks():
     assert p.returncode == 0
     assert exists(array_script)
 
-    for chunk in range(1, chunks+1):
-      os.environ['PBS_ARRAYID'] = str(chunk)
-      expected = '\n'.join(map(str, outputs[(chunk-1)*chunk_size:chunk*chunk_size])) + '\n'
-      array_pipe = command_pipe(array_script)
-      out, _ = array_pipe.communicate()
+    for chunk in range(1, chunks + 1):
+        os.environ['PBS_ARRAYID'] = str(chunk)
+        expected = '\n'.join(
+            map(str,
+                outputs[(chunk - 1) * chunk_size:chunk * chunk_size])) + '\n'
+        array_pipe = command_pipe(array_script)
+        out, _ = array_pipe.communicate()
 
-      assert array_pipe.returncode == 0, \
-        "Chunk {}: return code = {}".format(chunk, array_pipe.returncode)
-      assert out == expected, \
-        "Chunk {}: Expected {} but got {}".format(chunk, expected, out)
+        assert array_pipe.returncode == 0, \
+            "Chunk {}: return code = {}".format(chunk, array_pipe.returncode)
+        assert out == expected, \
+            "Chunk {}: Expected {} but got {}".format(chunk, expected, out)
 
-def test_run_qbatch_local_piped_commands(): 
-    cmds = "\n".join(["echo hello"]*24)
+
+def test_run_qbatch_local_piped_commands():
+    cmds = "\n".join(["echo hello"] * 24)
     p = command_pipe('qbatch -b local')
     out, err = p.communicate(cmds)
 
