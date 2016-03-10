7988698fdf580fdcb7fa71a3f325bfa4de8ec428
Jon Pipitone <jon@pipitone.ca>
2016-03-09 21:12:01 -0500

    Updated tests to use scripts/qbatch

--- test/test_qbatch.py
+++ test/test_qbatch.py
@@ -17,6 +17,7 @@ def setup_module():
     tempdir = tempfile.mkdtemp(dir=SHARED_FOLDER)
     os.environ["QBATCH_SCRIPT_FOLDER"] = tempdir
     os.environ["QBATCH_PPN"] = "1"
+    os.environ["PATH"] = "./scripts:" + os.environ["PATH"] # HACK
 
 def teardown_module(): 
     shutil.rmtree(tempdir)
