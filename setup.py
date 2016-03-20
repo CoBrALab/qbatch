3bc7a5814350461c8dcdf18cea0e3fb7a90dbf69
Jon Pipitone <jon@pipitone.ca>
2016-03-20 14:24:46 -0400

    Merged pbs_jobnames with qbatch
    This fixes #46

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 setup(
     name='qbatch',
     version='0.1',
-    description='A tool for submitting commands to SGE/PBS clusters',
+    description='Execute shell command lines in parallel on SGE/PBS clusters',
     author="Jon Pipitone, Gabriel A. Devenyi",
     author_email="jon@pipitone.ca, gdevenyi@gmail.com",
     license='Unlicense',
     url="https://github.com/pipitone/qbatch",
-    scripts=["bin/qbatch", "bin/pbs_jobnames"],
+    scripts=["bin/qbatch"],
     long_description=open('README.md').read(),
     setup_requires=['nose>=1.0'],
 )
