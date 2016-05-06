0d004ad85dcae34906fc06b7f4789276514b50e6
Jon Pipitone <jon@pipitone.ca>
2016-05-06 07:29:27 -0400

    Update setup.py for 1.0 release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except (IOError, ImportError):
 
 setup(
     name='qbatch',
-    version='1.0rc2',
+    version='1.0',
     description='Execute shell command lines in parallel on SGE/PBS clusters',
     author="Jon Pipitone, Gabriel A. Devenyi",
     author_email="jon@pipitone.ca, gdevenyi@gmail.com",
@@ -22,15 +22,14 @@ setup(
     scripts=["bin/qbatch"],
     setup_requires=['nose>=1.0'],
     classifiers=[
-       'Development Status :: 4 - Beta',
+       'Development Status :: 5 - Production/Stable',
        'Environment :: Console',
        'Intended Audience :: Science/Research',
        'License :: Public Domain',
        'Natural Language :: English',
        'Operating System :: POSIX :: Linux',
-       'Programming Language :: Python :: 2.6',
-       'Programming Language :: Python :: 2.7',
-       'Programming Language :: Python :: 2 :: Only',
+       'Programming Language :: Python :: 2',
+       'Programming Language :: Python :: 3',
        'Topic :: System :: Clustering',
        'Topic :: System :: Distributed Computing',
        'Topic :: Utilities',
