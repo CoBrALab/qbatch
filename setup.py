36d0cd840207ae624f4e14041c331922db60f110
Gabriel A. Devenyi <gdevenyi@gmail.com>
2020-03-12 09:17:17 -0400

    Update setup.py to new markdown readme support for long description in PyPi

--- setup.py
+++ setup.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from io import open
 
-# pypi doesn't like markdown
-# https://github.com/pypa/packaging-problems/issues/46
-try:
-    import pypandoc
-    description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    description = ''
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='qbatch',
     version='2.2',
-    description='Execute shell command lines in parallel on Slurm, S(sun|on of) Grid Engine (SGE) and PBS/Torque clusters',
+    description='Execute shell command lines in parallel on Slurm, '
+    'S(un|on of) Grid Engine (SGE) and PBS/Torque clusters',
     author="Jon Pipitone, Gabriel A. Devenyi",
     author_email="jon@pipitone.ca, gdevenyi@gmail.com",
     license='Unlicense',
     url="https://github.com/pipitone/qbatch",
-    long_description=description,
-    entry_points = {
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
         "console_scripts": [
             "qbatch=qbatch:qbatchParser",
         ]
     },
     packages=["qbatch"],
     classifiers=[
-       'Development Status :: 5 - Production/Stable',
-       'Environment :: Console',
-       'Intended Audience :: Science/Research',
-       'License :: Public Domain',
-       'Natural Language :: English',
-       'Operating System :: POSIX :: Linux',
-       'Programming Language :: Python :: 2',
-       'Programming Language :: Python :: 3',
-       'Topic :: System :: Clustering',
-       'Topic :: System :: Distributed Computing',
-       'Topic :: Utilities',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: Utilities',
     ],
     install_requires=[
         "six",
